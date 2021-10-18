a9ee662dab1fff96fd8ceb79f4f40b5d8afa4764
Payal Desai <payal@idesai.com>
2021-10-18 14:42:24 -0400

    flipfunctionworks

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
 function flipCard() {
   this.classList.toggle('flip');
+
+  if(!hasFlippedCard) {
+    // in the first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log({hasFlippedCard, firstCard})
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
